29037ba7909c7d28e4fc361709fe5bc0793a254b
Bijin Chakraborty <14847418+bijinc@users.noreply.github.com>
2025-11-09 16:48:26 -0500

    populate with random names and exts

--- src/populate.rs
+++ src/populate.rs
@@ -1,13 +1,25 @@
 use std::path::Path;
 use std::fs;
+use rand::Rng;
 
 pub fn populate_dir(path: &Path) {
     println!("Populating dir: {}", path.display());
 
+    let num_files = rand::thread_rng().gen_range(10..=20);
+
+    let prefixes = vec!["image", "document", "music", "video", "archive"];
+    let extensions = vec!["txt", "log", "md", "jpg", "png", "mp3", "mp4", "zip"];
+
+    for i in 0..num_files {
+        let prefix = prefixes[rand::thread_rng().gen_range(0..prefixes.len())];
+        let extension = extensions[rand::thread_rng().gen_range(0..extensions.len())];
+        let filename = format!("{}_{}.{}", prefix, i, extension);
+        let file_path = path.join(&filename);
+        fs::write(&file_path, format!("This is file number {}", i)).unwrap();
+    }
+
     fs::create_dir_all(path.join("subdir1")).unwrap();
     fs::create_dir_all(path.join("subdir2")).unwrap();
-    fs::write(path.join("file1.txt"), "This is file 1").unwrap();
-    fs::write(path.join("file2.log"), "This is file 2").unwrap();
     fs::write(path.join("subdir1/file3.txt"), "This is file 3").unwrap();
     fs::write(path.join("subdir2/file4.md"), "This is file 4").unwrap();
 }
