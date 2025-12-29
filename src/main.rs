42a08c0f8667cc40f26644287e3c055d29808f69
Bijin Chakraborty <14847418+bijinc@users.noreply.github.com>
2025-12-29 22:48:25 +0530

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,6 @@ use cli::Cli;
 
 fn main() {
     let cli = Cli::parse();
-    
-    // Setup sandbox directory
-    // if cli.path.exists() {
-    //     println!("path: {}", cli.path.display());
-    // } else {
-    //     match fs::create_dir_all(&cli.path) {
-    //         Ok(_) => println!("Sandbox directory created: {}", cli.path.display()),
-    //         Err(e) => {
-    //             eprintln!("Failed to create sandbox directory: {}", e);
-    //             std::process::exit(1);
-    //         }
-    //     }
-    // }
 
     // Execute the command
     if let Err(e) = cli.command.execute() {
